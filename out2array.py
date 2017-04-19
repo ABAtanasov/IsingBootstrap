fecf7987816ca1901bc77cfd0ba51771976fb599
Alex Atanasov <alexander.atanasov@yale.edu>
2017-04-19 02:17:14 -0400

    new out2array script

--- out2array.py
+++ out2array.py
@@ -5,10 +5,10 @@ lines = sys.stdin.readlines()
 
 inclusion = re.compile("is not excluded")
 number_data = re.compile("[\d]+.[\d]+")
-thetas = False
+theta = False
 if len(sys.argv) > 2:
-    if sys.argv[2] == 'thetas':
-        thetas = True
+    if sys.argv[2] == 'theta':
+        theta = True
 
 
 sigmas = []
@@ -18,11 +18,12 @@ thetas = []
 for line in lines:
     if inclusion.search(line) is not None:
         data = number_data.findall(line)
+        if len(data) > 2: theta = True
         sigmas.append(float(data[0]))
         epsilons.append(float(data[1]))
-        if thetas: thetas.append(float(data[2]))
+        if theta: thetas.append(float(data[2]))
 
 
 print sigmas
 print epsilons
-if thetas: print thetas
+if theta: print thetas
