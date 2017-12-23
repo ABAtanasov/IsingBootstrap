d7ab2618210deb361a4f239c05a77d6a8d6487a7
Alex Atanasov <alexander.atanasov@yale.edu>
2017-12-23 02:02:09 -0500

    better clean file

--- clean.py
+++ clean.py
@@ -1,10 +1,18 @@
 import os
+import sys
 
 mainpath = os.path.dirname(__file__)
 scratchpath = os.path.join(mainpath, "scratch")
 
-def clear_ck(name):
-    os.system("rm scratch/{}*.ck", name)
+def clear_ck(name=""):
+    os.system("rm scratch/{}*.ck".format(name))
 
-def clear_xml(name):
-    os.system("rm scratch/{}*.xml", name)
+def clear_xml(name=""):
+    os.system("rm scratch/{}*.xml".format(name))
+
+if __name__ == "__main__":
+    if sys.argv[1] == "ck":
+        clear_ck()
+    elif sys.argv[1] == "xml":
+        clear_xml()
+
