acc287e45d8393b905e7d8d4f53df4f40f029c11
Alex Atanasov <alexander.atanasov@yale.edu>
2018-03-03 12:45:51 -0600

    added the ability to read bisection outputs to out2array

--- out2array.py
+++ out2array.py
@@ -8,6 +8,7 @@
 import sys
 import re
 import os
+import numpy as np
 from point_generator import generate_from_file
 
 def read_lines(lines, points, form="is not excluded"):
@@ -19,29 +20,63 @@ def read_lines(lines, points, form="is not excluded"):
             data = number_data.findall(inclusionstring.groups()[0])  # get the 2 or 3 coordinates of the point
             points.append(map(lambda x: float(x), data)) # add it as an array of floats
 
+
+def read_bisections(lines, points):
+    inclusion = re.compile("(\([-?\d.\d, ]+\)) is")  # regex compile for mixed_ising output
+    number_data = re.compile("[\d]+.[\d]+")
+    line = len(lines)-1
+    boundary_found = False
+    prev_point = None; prev_point2 = None; point = None
+    print "we have", line, "lines"
+    while line >=0:
+        inclusionstring = inclusion.search(lines[line])
+        if inclusionstring is None:
+            line -= 1   # In case we have errors in certain outputs, we won't break, we'll just keep going
+            continue
+
+        data = number_data.findall(inclusionstring.groups()[0]) # find the 2 or 3 coordinates of this point
+        prev_point2 = prev_point
+        prev_point = point
+        point = np.array(map(lambda x: float(x), data))
+        if boundary_found and prev_point2 is not None:
+            if np.linalg.norm(prev_point - prev_point2) > np.linalg.norm(point - prev_point):
+                boundary_found = False
+        if not boundary_found and 'not excluded' in lines[line]: # and 'not' not in lines[line]:
+            points.append((point))  # add it as an array of floats
+            boundary_found = True
+        line -= 1
+
+
 if __name__ == "__main__":
 
     points = []
+    envelope = False
+    bisect = False
     if len(sys.argv) > 1 and sys.argv[1] == "envelope":
         envelope = True
-    else:
-        envelope = False
+    if len(sys.argv) > 1 and sys.argv[1] == "bisect":
+        bisect = True
     # If one argument is supplied, it'll search through the out files
     # for all those matching that argument
     if len(sys.argv) > 1:
         form = "is not excluded"
         if envelope:
             form = "is excluded"
-
         for filename in os.listdir("out_files"):
             if (sys.argv[-1]+'_') in filename:
                 f_out = open("out_files/{}".format(filename), 'r')
-                read_lines(f_out, points, form=form)
+                if bisect:
+                    read_bisections(f_out.readlines(), points)
+                else:
+                    read_lines(f_out.readlines(), points, form=form)
                 f_out.close()
 
     # Otherwise we read from the file that is fed into the program
     else:
-        read_lines(sys.stdin.readlines(), points)
+        if bisect:
+            get_boundary()
+        else:
+            read_lines(sys.stdin.readlines(), points)
 
 
     if envelope:
